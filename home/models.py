09653c8c35159fdb4704f8d949547d67408308ac
jesuloba-world <jesulobajohn@gmail.com>
2023-02-27 14:44:41 +0100

    add image to hero

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from product.models import Product
+from django.core.exceptions import ValidationError
+from images.models import ProductImage
 
 
 class Hero(models.Model):
@@ -8,3 +10,12 @@ class Hero(models.Model):
         on_delete=models.CASCADE,
     )
     copy = models.TextField(null=False, blank=True)
+    image = models.OneToOneField(ProductImage, on_delete=models.CASCADE, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk and Hero.objects.exists():
+            raise ValidationError("There is can be only one Hero instance")
+        return super(Hero, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "The hero section"
