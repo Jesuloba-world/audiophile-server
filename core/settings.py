b06b97bbda0e2f4d16f7159d41b984c714d77c8c
jesuloba-world <jesulobajohn@gmail.com>
2023-03-01 15:14:57 +0100

    added url for frontend deploy

--- core/settings.py
+++ core/settings.py
@@ -178,7 +178,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    # "http://localhost:5173",
+    "https://audiophilefm.netlify.app/",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
